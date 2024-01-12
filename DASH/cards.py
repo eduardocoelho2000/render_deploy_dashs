68be58cc9fe9ac0498d385b86073935a81c276ec
Eduardo Coelho <92464970+eduardocoelho2000@users.noreply.github.com>
2024-01-12 15:15:39 -0300

    Add files via upload

---
+++ DASH/cards.py
@@ -0,0 +1,42 @@
+import dash_bootstrap_components as dbc
+from dash import html
+import dash 
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+card_content = [
+    dbc.CardHeader("Card header"),
+    dbc.CardBody(
+        [
+            html.H5("Card title", className="card-title"),
+            html.P(
+                "This is some card content that we'll reuse",
+                className="card-text",
+            ),
+        ]
+    ),
+]
+
+
+app.layout = html.Div([
+    dbc.Row([
+        dbc.Col([
+            dbc.Card(card_content,color='primary',inverse = 'True',style={"height":"90vh", "margin":"10px"})],sm = 4),
+        dbc.Col([
+            dbc.Row([
+            dbc.Col(dbc.Card(card_content,color='info',inverse = 'True')),
+            dbc.Col(dbc.Card(card_content,color='warning',inverse = 'True'))
+                ]),
+        dbc.Row([
+            dbc.Col(dbc.Card(card_content,color='warning',inverse = 'True')),
+            dbc.Col(dbc.Card(card_content,color='warning',inverse = 'True')),
+            dbc.Col(dbc.Card(card_content,color='warning',inverse = 'True'))
+                ])
+
+            ])
+    ]),
+
+])
+
+if __name__ == "__main__":
+    app.run_server(debug = True)
